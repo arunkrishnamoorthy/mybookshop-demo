d1a8557e884523eb1166798ce7806f2a6e0517ca
Arun Krishnamoorthy <arun.krishnamoorthy@aarini.com>
2023-04-21 10:24:58 +0000

    Issue with No DB Credentials resolved, no data output yet

--- bookstore/server.js
+++ bookstore/server.js
@@ -2,4 +2,9 @@ const cds = require('@sap/cds');
 
 cds.once('served',require('./srv/mashup'));
 
+cds.once('bootstrap',(app) => {
+    app.serve('/bookshop').from('@aarini/products');
+    app.serve('/reviews').from('@aarini/reviews');
+});
+
 module.exports = cds.server;
--- bookstore/srv/mashup.js
+++ bookstore/srv/mashup.js
@@ -1,15 +1,22 @@
 module.exports = async () => {
     const cds = require('@sap/cds');
+    const adminLog = cds.log('AdminLog');
     const db = cds.connect.to('db');
     const CatalogService = await cds.connect.to('CatalogService');
     const AdminService = await cds.connect.to('AdminService');
-    const ReviewService = await cds.connect.to('ReviewService');
-    const { Books } = db.entities('mybookshopdemo.products.db');
+    const ReviewService = await cds.connect.to('ReviewsService');
+    // const { Books } = db.entities('mybookshopdemo.products.db');
 
     CatalogService.prepend((srv) => {
         srv.on('READ', 'Books/reviews', (request) => {
             const [ id ] = request.params, { columns, limit } = request.query.SELECT;
             return ReviewService.read('Reviews',columns).limit(limit).where({subject: String(id)});
+        });
+    });
+
+    AdminService.prepend((srv) => {
+        srv.on('READ','Books', (request)=> {
+            adminLog.log('> delegating to the Admin Books service');
         });
     });
 
